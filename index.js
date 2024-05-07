fa3bf9e7f43d6e3e3331c04c60b4b2d6c2a7bf3d
alexaldearroyo <alexaldearroyo@gmail.com>
2024-05-07 16:51:54 +0200

    Make hex color appear in the center of row 5

--- index.js
+++ index.js
@@ -8,14 +8,28 @@ const randomColor = randomHex.generate();
 
 for (let i = 0; i < height; i++) {
   if (i >= 3 && i <= 5) {
-    console.log(
-      '#'.repeat(startBlankColumn - 1) +
-        ' '.repeat(endBlankColumn - startBlankColumn + 1) +
-        '#'.repeat(width - endBlankColumn),
-    );
+    if (i === 4) {
+      // Center the random color between startBlankColumn and endBlankColumn
+      const padding = Math.floor(
+        (endBlankColumn - startBlankColumn - randomColor.length) / 2,
+      );
+      console.log(
+        '#'.repeat(startBlankColumn - 1) +
+          ' '.repeat(padding) +
+          randomColor +
+          ' '.repeat(padding + (randomColor.length % 2 === 0 ? 0 : 1)) +
+          '#'.repeat(width - endBlankColumn),
+      );
+    } else {
+      // Print empty rows with spaces
+      console.log(
+        '#'.repeat(startBlankColumn - 1) +
+          ' '.repeat(endBlankColumn - startBlankColumn + 1) +
+          '#'.repeat(width - endBlankColumn),
+      );
+    }
   } else {
+    // Print full-width rows
     console.log('#'.repeat(width));
   }
 }
-
-console.log(randomColor);
